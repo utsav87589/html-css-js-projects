4377affd1a88ee17d0d6e58eb39e77771940e3f9
utsav87589 <gkundukundu987@gmail.com>
2024-05-17 17:43:59 -0400

    Counter app v1

---
+++ counter-app/index.js
@@ -0,0 +1,23 @@
+//to fetch our number from the html
+let count = document.getElementById("counterLabel").innerText;
+
+//here, we converted our data type to number because initial data type was string
+count = Number(count);
+
+
+// we update the value when the button is pressed
+document.getElementById("decreaseBtn").onclick = function(){
+    count -= 1;
+    // console.log(count, typeof count); this is to check our data type before updating the final value
+    document.getElementById("counterLabel").textContent = count;
+}
+
+document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    document.getElementById("counterLabel").textContent = count;
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count += 1;
+    document.getElementById("counterLabel").textContent = count;
+}
